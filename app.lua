56664dbb8cb22c89302ea8ebdad12463a24b2bf1
andre alves garzia <andre@andregarzia.com>
2017-06-26 21:14:01 -0300

    Added gpio state changer

--- app.lua
+++ app.lua
@@ -31,11 +31,38 @@ wifi.sta.config(sta_config)
 -- WORK: WAIT FOR TCP STUFF
 --
 -- Step 2 - Wait for TCP broadcasts
+--
+-- Auxiliary functions
+
+local function msg_to_table(str)
+    t={}
+    str:gsub(".",function(c) table.insert(t,c) end)
+    return t
+end
+
 local port = 8972
 local server = net.createServer(net.UDP)
 server:on("receive", function(server, pl)
-      print("received :"..pl)
-      server:send("Yes") 
+    print("received :"..pl)
+    -- receives 12-char string, one for each gpio.
+    -- 0 and 1 means set gpio to output and set high or low.
+    -- 3 means read, return the value.
+    -- 
+    -- the returned value is also a 12-char value where each value is the gpio state.
+    local t = msg_to_table(pl)
+    local out = ""
+    for i, v in ipairs(t) do
+        if v == 0 or v == 1 then
+            gpio.mode(i, gpio.OUTPUT)
+            gpio.write(i, v)
+            out = out .. v
+        else if v == 3 then
+            gpio.mode(i, gpio.INPUT)
+            out = out .. tostring(gpio.read(i))
+        end
+    end
+
+    server:send(out) 
    end
 )
 
